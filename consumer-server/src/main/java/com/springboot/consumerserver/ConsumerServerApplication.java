058a17cc1a4f0566d0f835c3f20a75a8eae2ce34
jinmu.zhou <870855698@qq.com>
2019-10-29 15:47:33 +0800

    springcloud分布式

--- consumer-server/src/main/java/com/springboot/consumerserver/ConsumerServerApplication.java
+++ consumer-server/src/main/java/com/springboot/consumerserver/ConsumerServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableDiscoveryClient //开启发现服务功能
+@EnableDiscoveryClient //开启发现服务功能，一启动就被服务端感知
 @SpringBootApplication
 public class ConsumerServerApplication {
 
@@ -16,7 +16,7 @@ public class ConsumerServerApplication {
     }
 
     @Bean
-    @LoadBalanced
+    @LoadBalanced //负载均衡
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
